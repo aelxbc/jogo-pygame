0d86d25d197684b730e2f00bea125e6a65c8f81e
Alex Corrêa <aelxbc@outlook.com>
2022-02-14 10:21:41 -0300

    Colisões

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame 
 from pygame.locals import *
 from sys import exit
+from random import randint
 
 
 pygame.init()
@@ -9,6 +10,9 @@ largura = 640
 altura = 480
 x = largura / 2
 y = altura / 2 
+
+x_blue = randint(40, 600) 
+y_blue = randint(50, 430)
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo')
@@ -42,8 +46,12 @@ while True:
         y += 20
 
     # desenhando na tela
-    pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50))
+    ret_red = pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50))
+    ret_blue = pygame.draw.rect(tela, (0, 0, 255), (x_blue, y_blue, 40, 50))
     
+    if ret_red.colliderect(ret_blue):
+        x_blue = randint(40, 600) 
+        y_blue = randint(50, 430)
 
 
     pygame.display.update()
